2aaa860f1b506401350cbc112327e7c2970c8c1b
Rémy Bouquet <rebouquet@airfrance.fr>
2016-10-13 08:22:57 +0200

    More configurable generation

--- src/main/java/com/jme3/bootmonkey/ProjectGenerator.java
+++ src/main/java/com/jme3/bootmonkey/ProjectGenerator.java
@@ -14,11 +14,12 @@ import java.util.*;
  * Very Basic project scaffolding tool for JME3.
  * 1. Clones a template repository into a local folder
  * 2. attempt to change lines in project files as described in the replacement.yml
- *      the file must be of that structure:
- *      [filePath]:
- *          [search1]: [replacement1]
- *          [search2]: [replacement2]
- *          ...
+ *      the file must be follow that structure:
+ *      replace:
+ *          [filePath]:
+ *              [search1]: [replacement1]
+ *              [search2]: [replacement2]
+ *              ...
  *  - filePath is the path to the file to modify from the project root.
  *  - searchN is a regular expression to search in the file
  *  - replacementN is the text that will replace the regexp match. You can use template variables in this replacement:
@@ -28,8 +29,14 @@ import java.util.*;
  *      - packageName: the new project base package.
  *
  * 3. Clean up
- *      delete the .git folder (we don't want to enforce a VCS but yeah...people should use one, so maybe later on)
- *      moves the Main.java file to the proper application package.
+ *      Will copy and delete files according to what's found in the yaml configuration file
+ *      the file must be follow that structure:
+ *      copy:
+ *          [srcPath]: [destPath]
+ *          ...
+ *      delete:
+ *          - [fileORDirectoryPath]
+ *          - ...
  *
  * The generate method will return a list of errors if any.
  * You can add GenerationListener that will be called whenever the generation process reaches a new step.
@@ -38,6 +45,8 @@ import java.util.*;
 public class ProjectGenerator {
 
     private Map<String, Map<String, String>> replacements;
+    private Map<String, String> copies;
+    private List<String> deletes;
     private String tmpContent;
     private StrSubstitutor sub;
     private List<String> lastGenerationErrors = new ArrayList<>();
@@ -45,7 +54,18 @@ public class ProjectGenerator {
 
     public ProjectGenerator() {
         Yaml yaml = new Yaml();
-        replacements = (Map<String, Map<String, String>>) yaml.load(this.getClass().getResourceAsStream("/replacement.yml"));
+        Map<String, Object> map = (Map<String, Object>)yaml.load(this.getClass().getResourceAsStream("/replacement.yml"));
+
+        if (map.get("replace") != null) {
+            replacements = (Map<String, Map<String, String>> )map.get("replace");
+        }
+        if (map.get("copy") != null) {
+            copies = (Map<String, String>)map.get("copy");
+        }
+        if (map.get("delete") != null) {
+            deletes = (List<String>)map.get("delete");
+        }
+
     }
 
 
@@ -72,6 +92,8 @@ public class ProjectGenerator {
         String baseDir = params.get("baseDir");
         String projectName = params.get("projectName");
         String packageName = params.get("packageName");
+        String packagePath = packageName.replaceAll("\\.", "/");
+        params.put("packagePath", packagePath);
         String projectPath = baseDir + projectName + "/";
 
         progress("Cloning template project...");
@@ -81,7 +103,7 @@ public class ProjectGenerator {
         replacements.entrySet().forEach((e) -> adaptFile(projectPath + e.getKey(), e.getValue()));
 
         progress("Cleaning up...");
-        moveAndCleanupFiles(baseDir, projectName, packageName, projectPath);
+        moveAndCleanupFiles(projectPath);
 
         if(lastGenerationErrors.isEmpty()) {
             progress("Done.");
@@ -91,14 +113,21 @@ public class ProjectGenerator {
         return Collections.unmodifiableList(lastGenerationErrors);
     }
 
-    private void moveAndCleanupFiles(String baseDir, String projectName, String packageName, String projectPath) {
+    private void moveAndCleanupFiles(String projectPath) {
         try {
-            FileUtils.moveFile(new File(projectPath + "src/main/java/mygame/Main.java"), new File(projectPath + "src/main/java/" + packageName.replaceAll("\\.", "/") + "/Main.java"));
-            //delete .git directory
-            FileUtils.deleteDirectory(new File(baseDir + projectName + "/src/main/java/mygame"));
-            FileUtils.deleteDirectory(new File(baseDir + projectName + "/.git"));
+            for (Map.Entry<String, String> copy : copies.entrySet()) {
+                FileUtils.copyFile(new File(projectPath + copy.getKey()), new File(projectPath + sub.replace(copy.getValue())));
+            }
         } catch (IOException e) {
             reportError("Error while moving and cleaning up", e);
+        }
+
+        for (String delete : deletes) {
+            String subDelete = sub.replace(delete);
+            boolean deleted = FileUtils.deleteQuietly(new File(projectPath + subDelete));
+            if(!deleted){
+                reportError("Error while deleting " + subDelete, new Exception(delete));
+            }
         }
     }
 
@@ -120,7 +149,7 @@ public class ProjectGenerator {
                 tmpContent = tmpContent.replaceAll(key, finalValue);
             });
 
-          //  System.err.println(tmpContent);
+//System.err.println(tmpContent);
             FileUtils.writeStringToFile(new File(fileName), tmpContent, "UTF-8");
         } catch (IOException e) {
             reportError("Issue adapting file " + fileName,  e);
